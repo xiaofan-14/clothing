0c8f46427ee43619a0dfee5fe848b2b964d1dcff
xiaofan <ningfengzeng@gmail.com>
2025-09-29 15:28:56 +0800

    feat: add logo

--- packages/apps/src/components/home/header-bar.vue
+++ packages/apps/src/components/home/header-bar.vue
@@ -2,6 +2,9 @@
 </script>
 
 <template>
-  <header class="bg-white px-4 py-6 shadow-sm">
+  <header class="bg-white text-gray-600 px-4 pt-4 flex justify-center items-center">
+    在线
+    <img src="@/assets/shopper.png" class="w-10 h-10 mx-2" />
+    服饰
   </header>
 </template>
